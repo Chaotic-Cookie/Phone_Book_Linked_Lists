5492d0b21d08182b210767a2ab1e2259173fef61
mangamariam <mangamariam2001@gmail.com>
2020-05-07 16:01:52 -0700

    added starts to control portions

--- src/phoneBookManager.java
+++ src/phoneBookManager.java
@@ -2,7 +2,7 @@
  * Mariam Khatib
  * Lab: Phonebook/Linked Lists
  * Spring 2020
- *
+ *IDE: IntelliJ
  */
 
 
@@ -10,5 +10,21 @@ public class phoneBookManager {
     //needs to add an entry (name, address, city, phone #) to the end and then sort it, or alphabetically by last name
     // modify the entry, delete entries, or move them from the Bellingham to the Seattle phone book (optional)
     //print out your list in a nice format
+
+
+    private phonebook_node start;
+
+    public phoneBookManager(){ //creates an empty node
+        start = null;
+    }
+
+
+    public void add(){}
+    public void delete(){}
+    public void searchEmail(){}
+    public void searchName(){}
+    public void sort(){}
+    public void print(){}
+
 
 }
